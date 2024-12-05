17ed553b8370c6d5175e5781f3ec7a8252d72a91
Bert Derijckere <b.derijckere@vmm.be>
2024-12-05 09:48:03 +0100

    add libraryGenerator

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -6,19 +6,25 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
+import { libraryGenerator } from "@nx/js"
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
   const projectRoot = `libs/${options.name}`;
+  await libraryGenerator(tree, {
+    directory: options.name,
+  });
+
+  /*const projectRoot = `libs/${options.name}`;
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
     projectType: 'library',
     sourceRoot: `${projectRoot}/src`,
     targets: {},
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);*/
   await formatFiles(tree);
 }
 
