0bba2dfe16404a02f4610f943e558d260ae0cb85
Bert Derijckere <b.derijckere@vmm.be>
2024-12-05 16:08:13 +0100

    WIP add implementation for fly-deploy

--- libs/internal-plugin/src/executor/executor.ts
+++ libs/internal-plugin/src/executor/executor.ts
@@ -1,5 +1,6 @@
 import { PromiseExecutor } from '@nx/devkit';
 import { FlyDeployExecutorSchema } from './schema';
+import { execSync } from 'child_process';
 
 const runExecutor: PromiseExecutor<FlyDeployExecutorSchema> = async (
   options
@@ -7,9 +8,33 @@ const runExecutor: PromiseExecutor<FlyDeployExecutorSchema> = async (
   console.log(
     `Running FlyDeploy for app ${options.name} from dist folder ${options.distPath}`
   );
-  return {
-    success: true,
-  };
+  const cwd = options.distPath;
+  const results = execSync(`flyctl apps list`);
+  console.log(results.toString());
+
+  try {
+    if (results.toString().includes(options.name)) {
+      console.log('flyctl deploy');
+      execSync(`flyctl deploy --depot=false`, {
+        cwd,
+        stdio: 'inherit',
+      });
+    } else {
+      console.log('flyctl launch');
+      // consult https://fly.io/docs/reference/regions/ to get best region for you
+      execSync(
+        `flyctl launch --depot=false --now --name=${options.name} --yes --copy-config --region=lax`,
+        {
+          cwd,
+          stdio: 'inherit',
+        }
+      );
+    }
+    return { success: true };
+  } catch (error) {
+    console.error('Deployment failed:', error);
+    return { success: false };
+  }
 };
 
 export default runExecutor;
